b0c43122eec7f0be102d3ed5b901896adc9911da
Rawle Becerril <rawlesxm@gmail.com>
2023-02-01 21:22:51 -0500

    gameplay functionality built

--- landscaper.py
+++ landscaper.py
@@ -2,7 +2,7 @@
 
 ## Game State
 game = {"tool": 0, "money": 0}
-money = 0
+
 tools = [
     {"name": "teeth", "profit" : 1, "cost": 0},
     {"name": "rusty scissors", "profit" : 5, "cost": 5},
@@ -10,16 +10,47 @@ tools = [
     {"name": "battery-powered mower", "profit" : 100, "cost": 250},
     {"name": "team", "profit" : 250, "cost": 500},
     ]
+
+def cutGrass():
+    tool = tools[game["tool"]]
+    print(f'you cut the grass with your {tool["name"]} and make {tool["profit"]}')
+    game["money"] += tool["profit"]
+    
+def checkStats():
+    tool = tools[game["tool"]]
+    print(f'You have ${game["money"]} in your account and are using a {tool["name"]}')
+    
+def upgrade():
+    nextTool= tools[game["tool"] + 1]
+    if (nextTool == None):
+        print("There are no more tools available")
+        return 0
+    if (game["money"] < nextTool["cost"]):
+        print("not enough money to buy tool!")
+        return 0
+    game["money"] -= nextTool["cost"]
+    game["tool"]  += 1
+    
+def winCheck():
+    if(game["tool"] == 1 and game["money" == 1000]):
+        print("You Win!")
+        return True
+    return False
+    
+    
+    
 uinput = input("Welcome to Landscaper! Enter 'Start' to begin, enter 'Q' to Quit")
 
+
 if (uinput == 'Start'):
-    if (money < 5):
-        print(f'You have ${money} in your account')
-    if (money < 25):
-       print(f'You have ${money} in your account')
-    if (money < 250):
-        print(f'You have ${money} in your account')
-    if (money < 500):
-        print(f'You have ${money} in your account')
+    if (game["money"] < 5):
+        print(f'You have ${game["money"]} in your account')
+        workinput = input(f"You have")
+    if (game["money"] < 25):
+       print(f'You have ${game["money"]} in your account')
+    if (game["money"] < 250):
+        print(f'You have ${game["money"]} in your account')
+    if (game["money"] < 500):
+        print(f'You have ${game["money"]} in your account')
 pass
     
