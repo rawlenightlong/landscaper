9be608c2ca9a4a872c8f4cca7258abd3792fe20f
Rawle Becerril <rawlesxm@gmail.com>
2023-02-01 21:04:35 -0500

    set up game state and tools

--- landscaper.py
+++ landscaper.py
@@ -1,1 +1,16 @@
 # Landscaper
+
+## Game State
+game = {"tool": 0, "money": 0}
+money = 0
+tools = [
+    {"name": "teeth", "profit" : 1, "cost": 0},
+    {"name": "rusty scissors", "profit" : 5, "cost": 5},
+    {"name": "old-timey push mower", "profit" : 50, "cost": 25},
+    {"name": "battery-powered mower", "profit" : 100, "cost": 250},
+    {"name": "team", "profit" : 250, "cost": 500},
+    ]
+uinput = input("Welcome to Landscaper! Enter 'Start' to begin, enter 'Q' to Quit")
+
+
+    
