a6ca6d7c7f860dbe5765cb34e5ca711ca06affbb
Rawle Becerril <rawlesxm@gmail.com>
2023-02-01 22:35:58 -0500

    finished game

--- landscaper.py
+++ landscaper.py
@@ -4,11 +4,11 @@
 game = {"tool": 0, "money": 0}
 
 tools = [
-    {"name": "teeth", "profit" : 1, "cost": 0},
-    {"name": "rusty scissors", "profit" : 5, "cost": 5},
+    {"name": "set of teeth", "profit" : 1, "cost": 0},
+    {"name": "pair of rusty scissors", "profit" : 5, "cost": 5},
     {"name": "old-timey push mower", "profit" : 50, "cost": 25},
     {"name": "battery-powered mower", "profit" : 100, "cost": 250},
-    {"name": "team", "profit" : 250, "cost": 500},
+    {"name": "team of starved college kids", "profit" : 250, "cost": 500},
     ]
 
 # Game Option Functions
@@ -17,10 +17,11 @@ def cutGrass():
     tool = tools[game["tool"]]
     print(f'You cut the grass with your {tool["name"]} and make ${tool["profit"]}')
     game["money"] += tool["profit"]
+    print(f"You have ${game['money']}")
     
 def checkStats():
     tool = tools[game["tool"]]
-    print(f'You have ${game["money"]} in your account and are using a {tool["name"]}')
+    print(f'You have ${game["money"]} in your account and are using the {tool["name"]}')
     
 def upgrade():
     nextTool = tools[game["tool"] + 1]
@@ -28,18 +29,27 @@ def upgrade():
         print("There are no more tools available")
         return 0
     if (game["money"] < nextTool["cost"]):
-        print("Not enough money to buy tool!")
+        print(f"Not enough money to buy the {nextTool['name']} !")
         return 0
     game["money"] -= nextTool["cost"]
     game["tool"]  += 1
+    print(f"You bought the {nextTool['name']} for ${nextTool['cost']}! You now have ${game['money']}!") 
+    if(game['tool'] == 4):
+        print("There are no more tools to buy!")
+    #if (nextTool != None):
+     #   print(f"Your next tool is the {tools[game['tool'] + 1]['name']} and costs ${tools[game['tool'] + 1]['cost']}!")
+    #else:
+      #  return True
     
 def winCheck():
-    if(game["tool"] == 4 and game["money" == 1000]):
+    if(game["tool"] == 4 and game["money"] >= 1000):
         print("You Win!")
         return True
     return False
-    
+
+print("Welcome to Landscaper! Pick an option:")    
 while(True):
+    
     i = input(" [1] Cut Grass [2] Check Stats [3] Upgrade [4] Quit") 
     i = int(i)
     
